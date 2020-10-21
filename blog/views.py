3717a9305c8dd5218b3ee366695990e444f4c7e4
Vinícius Melo <vinimmelo.dev@gmail.com>
2020-10-20 22:00:55 -0300

    Adicionado templates e views
    - Projeto django girls

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,41 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Post
+from .forms import PostForm
 
 
 def post_list(request):
-    return render(request, "blog/post_list.html", {})
+    # GET
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by(
+        "published_date"
+    )
+    return render(request, "blog/post_list.html", {"posts": posts},)
+
+
+def post_detail(request, pk):
+    # GET
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, "blog/post_detail.html", {"post": post})
+
+
+def post_new(request):
+    return _post_new_edit(request)
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return _post_new_edit(request, post)
+
+
+def _post_new_edit(request, post=None):
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post) if post else PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect("post_detail", pk=post.pk)
+    else:
+        form = PostForm(instance=post) if post else PostForm()
+    return render(request, "blog/post_edit.html", {"form": form})
